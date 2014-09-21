cafa9c9b2648eb24c89963b7382c91cddd4abe1a
YG <sadandrea@163.com>
2014-09-21 14:28:46 +0800

    synchronize JinGe modified

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,23 +4,12 @@ jQuery(function() {
 		$('body').toggleClass('body-push-toleft');
 		$('#theMenu').toggleClass('menu-open');
 	});
-	$("#timecounter").jCountdown({
-		timeText: "2014/10/02 11:30:00",
-		timeZone: 8,
-		style: "metal",
-		color: "black",
-		width: "auto",
-		textGroupSpace: 15,
-		textSpace: 0,
-		reflection: true,
-		reflectionOpacity: 10,
-		reflectionBlur: 0,
-		dayTextNumber: 2,
-		displayDay: true,
-		displayHour: true,
-		displayMinute: true,
-		displaySecond: true,
-		displayLabel: true,
-		onFinish: function() {}
-	});
+    var now = new Date().getTime() / 1000;
+    var end = new Date("2014/10/02 11:30:00").getTime() / 1000;
+
+	var clock = $('.myclock').FlipClock(end - now, {
+        clockFace: 'DailyCounter',
+        countdown: true
+
+    });
 });
